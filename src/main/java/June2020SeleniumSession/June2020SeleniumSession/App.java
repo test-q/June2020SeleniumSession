6494aa019b74729bc339c289212da5ebc7d1ee4d
test-q <sr.test85@gmail.com>
2020-07-16 13:55:43 +0530

    First Selenium Project

---
+++ src/main/java/June2020SeleniumSession/June2020SeleniumSession/App.java
@@ -0,0 +1,13 @@
+package June2020SeleniumSession.June2020SeleniumSession;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
