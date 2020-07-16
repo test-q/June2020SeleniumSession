5b3fe78701b812453e35113dfe66c2399bda7dbc
test-q <sr.test85@gmail.com>
2020-07-16 16:38:50 +0530

    Update WebDriver Basic

--- src/main/java/SeleniumSession/WebDriverBasics.java
+++ src/main/java/SeleniumSession/WebDriverBasics.java
@@ -6,12 +6,48 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class WebDriverBasics {
 
 	public static void main(String[] args) {
+		/* Browser Company upgrade there browser and version change continuously hence selenium facing Compatibility issues so it is decided every browser company  
+		   will provide one executable file ie .exe and you have configure this .exe file in your code in the form of System.setProperty and than launch the browser
+		   and this is the major change selenium 2 to selenium 3
+		 */
+		System.setProperty("webdriver.chrome.driver", "D:\\Rupali\\Driver\\chromedriver.exe");
 		
-		// WebDriver is interface hence we can not create object of WebDriver
-		// Hence we are doing Top casting -> Create object of child class can be
-		// referred by Interface reference variable
-		WebDriver driver = new ChromeDriver();
-		driver.get("http://google.com");
+//		WebDriver driver = new WebDriver(); //WebDriver is interface and we can not create object of WebDriver
+//		Hence we are doing Top casting -> Create object of child class can be referred by Interface reference variable
+		WebDriver driver = new ChromeDriver();//ChromeDriver is class which is extending RemoteWebDriver and RemoteWebDriver is implementing WebDriver interface
+		                                      // i.e. WebDriver is grand Interface of ChromeDriver class
+		                                      // We are creating object that means ChromeDriver constructor will be called.
+											  // and inside the default constructor selenium guys written code for launch browser 
+											  // Hence this line launch the browser.
+											  //If you not written System.setProperty line than it will throw the "ExecutionException" error
+		
+		driver.get("http://google.com");      //Enter URL - If u r written this way "www.google.com" than it is not working ->Without http it wont work
+		
+		String title1 = driver.getTitle();    // Getting title
+		System.out.println(title1);
+//		driver.get("https://www.amazon.com/");
+//		String title2 = driver.getTitle();
+//		System.out.println(title2);
+		
+		//Validation point/ check point
+		
+		if(title1.equals("Google")) {
+			System.out.println("Title is correct.");
+		}else {
+			System.out.println("Title is not correct.");
+		}
+		
+		String url = driver.getCurrentUrl();
+		System.out.println(driver.getCurrentUrl());
+		if(url.contains("google")) {
+			System.out.println("Correct URL");
+		}else{
+			System.out.println("Incorrect URL");
+		}
+		
+		//System.out.println(driver.getPageSource());
+		//Close the browser
+		driver.quit();
 
 	}
 
