dffedbcd4e5ac097ea665ac5ef2f0449c9c15c47
test-q <sr.test85@gmail.com>
2020-07-23 08:10:33 +0530

    Find out total links and images

---
+++ src/main/java/SeleniumSession/TotalImages.java
@@ -0,0 +1,38 @@
+package SeleniumSession;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class TotalImages {
+
+	public static void main(String[] args) {
+		
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://www.amazon.in/");
+		List<WebElement> imageList = driver.findElements(By.tagName("img"));
+		System.out.println(imageList.size());
+		
+//		for(int i=0; i<imageList.size(); i++){
+//			String imgAttri = imageList.get(i).getAttribute("src");
+//			System.out.println(imageList);
+//		}
+		
+//		for(WebElement ele : imageList) {
+//			System.out.println(ele.getAttribute("src"));
+//		}
+		
+		imageList.stream().forEach(ele -> System.out.println(ele.getAttribute("src")));
+		
+		driver.close();
+
+	}
+
+}
