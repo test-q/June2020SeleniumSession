f39cce99bf0d4a20d38aa852939298ed0ba2a7d4
test-q <sr.test85@gmail.com>
2020-08-12 19:57:37 +0530

    pom.xml file updated

--- src/main/java/SeleniumSession/AmazonTest.java
+++ src/main/java/SeleniumSession/AmazonTest.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class AmazonTest {
-
+	
 	public static void main(String[] args) throws InterruptedException {
 	
 		String browserName = "chrome";
